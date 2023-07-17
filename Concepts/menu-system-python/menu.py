1a4b73b13229fc3fc60efbdc6c1bc762291c18d7
Daniel Mironov <mail@danielmironov.dev>
2023-07-18 01:29:53 +0200

    added some logic to menu system concept

--- Concepts/menu-system-python/menu.py
+++ Concepts/menu-system-python/menu.py
@@ -7,53 +7,44 @@ class MenuEmptyException(Exception):
 
 @dataclass
 class MenuItem:
-    _title: str
-    _callback: Callable
-    _is_current: bool
-    _is_focused: bool
+    _title: str = field(default="")
+    _callback: Callable = field(default=None)
+    _is_current: bool = field(default=False)
+    _is_focused: bool = field(default=False)
 
-    @property
-    def title(self) -> str:
+    def get_title(self) -> str:
         return self._title
 
-    @title.setter
-    def title(self, new_title: str) -> None:
+    def set_title(self, new_title: str) -> None:
         self._title = new_title
 
-    @property
     def is_current(self) -> bool:
         return self._is_current
 
-    @is_current.setter
-    def is_current(self, value: bool) -> None:
+    def set_current(self, value: bool) -> None:
         self._is_current = value
 
-    @property
     def is_focused(self) -> bool:
         return self._is_focused
 
-    @is_focused.setter
-    def is_focused(self, value: bool) -> None:
+    def set_focused(self, value: bool) -> None:
         self._is_focused = value
 
 
 
 @dataclass
 class BoolMenuItem(MenuItem):
-    _value: bool
+    _value: bool = field(default=False)
 
-    @property
-    def value(self) -> bool:
+    def get_value(self) -> bool:
         return self._value
 
-    @value.setter
-    def value(self, value: bool) -> None:
+    def set_value(self, value: bool) -> None:
         self._value = value
 
 
 @dataclass
 class AppMenuItem(MenuItem):
-
     def execute(self) -> None:
         self._callback()
 
@@ -65,19 +56,23 @@ class Menu:
 
     def add_item(self, item: MenuItem) -> None:
         self._items.append(item)
+        if len(self._items) == 1:
+            self._items[self._current_item].set_current(True)
 
     def next(self) -> None:
+        self.current_item().set_current(False)
         self._current_item = (self._current_item + 1) % len(self._items)
+        self.current_item().set_current(True)
 
     def prev(self) -> None:
-        num_elem = len(self._items)
-        self._current_item = (((self._current_item - 1) % num_elem) + num_elem) % num_elem
+        self.current_item().set_current(False)
+        length = len(self._items)
+        self._current_item = (((self._current_item - 1) % length) + length) % length
+        self.current_item().set_current(True)
 
-    @property
-    def items(self) -> list[MenuItem]:
+    def get_items(self) -> list[MenuItem]:
         return self._items
 
-    @property
     def current_item_index(self) -> int:
         return self._current_item
 
@@ -85,3 +80,18 @@ class Menu:
         if len(self._items) == 0:
             raise MenuEmptyException
         return self._items[self._current_item]
+
+    def render(self) -> str:
+        print("------------")
+        for item in self._items:
+            print("> " if item.is_current() else "  ", end="")
+            print(item.get_title())
+
+@dataclass
+class MenuManager:
+    _root_menu: Menu
+    _cur_menu: Menu
+
+    def get_root_menu(self) -> Menu:
+        return self._root_menu
+
